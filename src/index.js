400b0c6a9ac8efd5c4fe6969e88cf461cd5694e8
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-01-15 16:14:17 -0300

    projeto final do video

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
-const app = express();
 const routes = require('./routes.js');
+const cors = require('cors');
+
+const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-pvxkk.gcp.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
