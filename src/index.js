43c129016a924055198ebc664a54a79d9e3bc7bf
Adam Axel Augustinsky <40921659+AdamAugustinsky@users.noreply.github.com>
2020-01-17 15:39:17 -0300

    Adiciona e usa o socket.io

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
 const routes = require('./routes.js');
+const http = require('http');
 const cors = require('cors');
+const { setupWebSocket} = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-pvxkk.gcp.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -15,4 +20,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
